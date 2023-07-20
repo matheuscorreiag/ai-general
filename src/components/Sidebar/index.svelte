2d0189b872df4154591cfa29d756713b1d68570d
Matheus Correia <matheuscorreiags@gmail.com>
2023-07-20 03:07:02 -0300

    feat: configure tailwind, sidebar animation

---
+++ src/components/Sidebar/index.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { ChevronLeft } from 'lucide-svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	let sidebarOpen = false;
+</script>
+
+<div
+	class={twMerge(
+		'border-r border-r-zinc-600 h-screen relative ease-in-out duration-500',
+		sidebarOpen && 'w-72',
+		!sidebarOpen && 'w-10'
+	)}
+>
+	<button
+		class={twMerge(
+			'absolute right-4 top-4 cursor-pointer',
+			!sidebarOpen && 'right-0 w-full flex justify-center'
+		)}
+		on:click={() => (sidebarOpen = !sidebarOpen)}
+	>
+		<ChevronLeft
+			class={twMerge(
+				'transition-transform',
+				sidebarOpen && 'rotate-0',
+				!sidebarOpen && 'rotate-180'
+			)}
+			font-weight="bold"
+		/>
+	</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Sidebar from '../components/Sidebar/index.svelte';
+</script>
+
+<div class="bg-zinc-900 w-screen h-screen text-white">
+	<Sidebar />
+</div>
