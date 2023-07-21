5816fe0a2f5fcb70ab6c1952dff542de57a3bb97
Matheus Correia <matheuscorreiags@gmail.com>
2023-07-20 22:47:08 -0300

    feat: conversation api skeleton

---
+++ src/components/Chats/ConversationChat/Message.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Home } from 'lucide-svelte';
+
+	export let userType: 'AI' | 'USER' = 'USER';
+</script>
+
+{#if userType === 'USER'}
+	<div class="flex items-end justify-end">
+		<div class="flex w-fit flex-row rounded-full p-2">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum at cupiditate ullam aperiam
+				iste quas vitae atque eveniet facere? Blanditiis, sit illo eius vitae inventore ex veniam
+				numquam libero tempore?
+			</p>
+			<div class="mr-6 flex w-fit rounded-full">
+				<Home />
+			</div>
+		</div>
+	</div>
+{:else}
+	<div class="flex flex-row">
+		<div class="mr-6 flex w-fit rounded-full">
+			<Home />
+		</div>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum at cupiditate ullam aperiam
+			iste quas vitae atque eveniet facere? Blanditiis, sit illo eius vitae inventore ex veniam
+			numquam libero tempore?
+		</p>
+	</div>
+{/if}
---
+++ src/components/Chats/ConversationChat/index.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Input from './Input.svelte';
+	import Message from './Message.svelte';
+	import Title from './Title.svelte';
+</script>
+
+<div class="h-full">
+	<Title />
+	<Input />
+
+	<div class="mt-10 h-4/6 flex-col space-y-12 overflow-y-scroll rounded-lg bg-zinc-800 p-10">
+		<Message userType="AI" />
+		<Message userType="USER" />
+		<Message userType="AI" />
+		<Message userType="USER" />
+		<Message userType="AI" />
+		<Message userType="USER" />
+		<Message userType="AI" />
+		<Message userType="USER" />
+	</div>
+</div>
